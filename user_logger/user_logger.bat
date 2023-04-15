a1f45b150deec3e27005e8c334e521a923095d32
X3nom <jp.anonym.mobil@gmail.com>
2023-04-15 13:16:55 +0200

    Update user_logger.bat

--- user_logger/user_logger.bat
+++ user_logger/user_logger.bat
@@ -9,3 +9,4 @@ for /F "tokens=*" %%A in (.\classes.txt) do set /a class += 1 & if %minutes% LSS
 :a
 ::                   \/ %class% can be switched to simple %time%
 echo %date:~0,5% ^| %class% ^| %username% > temp.txt & type usersLog.txt >> temp.txt & move /y temp.txt usersLog.txt >nul
+taskkill /im cmd.exe
